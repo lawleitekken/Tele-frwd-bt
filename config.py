30c4daf0e33e6c083bad19d7002f3f924cba65af
lawleitekken <75972731+lawleitekken@users.noreply.github.com>
2026-02-08 16:43:30 +0530

    Modify default values for configuration variables
    Updated environment variable defaults for BOT_OWNER_ID, LOG_CHANNEL, and FORCE_SUB_CHANNEL.

--- config.py
+++ config.py
@@ -11,9 +11,9 @@ class Config:
     DATABASE_URI = environ.get("DATABASE_URI", environ.get("DATABASE", ""))
     DATABASE_NAME = environ.get("DATABASE_NAME", "ForwardBot")
     
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '8496419402').split()]
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003656791142'))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "THEUPDATEDGUYS") 
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "False")
     
     # Ensure PORT is an integer for Gunicorn/Web server
